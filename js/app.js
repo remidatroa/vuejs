87ccd3f6e03e9d248fbaa9fa7399ef706503715c
remidatroa <remidatroa@gmail.com>
2018-01-15 14:23:25 +0100

    form done message name var

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 new Vue ({
   el: '#app',
   data: {
-    message: 'hi bruh',
+    message: "",
     link: 'http://www.remidatroa.com',
     success: true,
     tags: ['full stack', 'front-end', 'graphic','motion']
